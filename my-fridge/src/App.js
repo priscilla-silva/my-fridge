1d79bd0b6bcd63f02ab9465ee968b602f74c0bb1
Priscilla Silva <priscilla.sg91@gmail.com>
2022-01-16 01:16:31 +0100

    login style done

--- my-fridge/src/App.js
+++ my-fridge/src/App.js
@@ -1,11 +1,14 @@
 import Login from "./comps/Login";
 
 
+
 function App() {
   return (
     <div className="App">
+      <div className="main">
       <h1>My Fridge</h1>
       <Login />
+      </div>
     </div>
   );
 }
--- my-fridge/src/comps/Login.js
+++ my-fridge/src/comps/Login.js
@@ -3,12 +3,14 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 
+
 const Login = () => {
 
     return (
     <div className="login-form">
         <TextField id="outlined-basic" label="E-mail" variant="outlined" />
         <Button id="login-btn" variant="contained">Login</Button>
+        <h4>Don't have an account yet? Sign up for free!</h4>
     </div>
     )
 }
